84073868e4c94d71a63265582d5bcb8b82f41bbd
Fabian Guarin <91294409+IngFabian97@users.noreply.github.com>
2026-08-18 22:30:14 -0500

    Fix lambda test response handling

--- tests/LambdaClima.spec.ts
+++ tests/LambdaClima.spec.ts
@@ -1,18 +1,13 @@
 import { test, expect } from '@playwright/test';
+import { LambdaInvoker } from '../utils/LambdaInvoker.js';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Probando flujo positivo del lambda Clima', async ({ }) => {
+  const lambdaInvoker = new LambdaInvoker();
+  const lambdaResponse = await lambdaInvoker.invokeLambda("Clima", { city: "Cúcuta" });
+  expect(lambdaResponse).toBeDefined();
+  expect(lambdaResponse.error).toBeNull();
+  const responseBody = lambdaResponse.body;
+  expect(responseBody.temperature).toBeGreaterThan(0);
+  console.log("Temperatura:", responseBody.temperature);
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+})
